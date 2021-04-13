90488715de91b1b67a44ffceef9648fad9f6ecad
Jan Horak <horak.jan@live.com>
2021-04-13 23:36:23 +0200

    Form for booking

---
+++ client/src/components/ui/Car/CarImageSlider.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Slider from "infinite-react-carousel";
+
+const CarImageSlider = (props) => {
+  const settings = {
+    arrows: false,
+    autoplay: false,
+    pauseOnHover: false,
+    swipe: false,
+    dots: true,
+  };
+  //style setting for carousel
+  const StyleSettings = {
+    width: "90vw",
+    maxWidth: "1000px",
+    height: "50vh",
+    margin: "2vh auto",
+  };
+
+  return (
+    <div className="ready-slider">
+      <Slider {...settings}>
+        <div className="ready-slider-slide-one">
+          <img src={props.images[0]} style={StyleSettings} />
+        </div>
+        <div className="ready-slider-slide-two">
+          <img src={props.images[1]} style={StyleSettings} />
+        </div>
+        {/* <div className="ready-slider-slide-three">
+          <img src={Image3} style={StyleSettings} />
+          <div style={DivStyle}></div>
+        </div> */}
+      </Slider>
+    </div>
+  );
+};
+
+export default CarImageSlider;
