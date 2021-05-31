e3ea3e4c8151046b53bd727a7bf281411424f1a4
Jan Horak <horak.jan@live.com>
2021-05-31 19:38:50 +0200

    Fixed hiding form to show message after sending form

--- client/src/components/ui/Pobocka/PobockaForm.js
+++ client/src/components/ui/Pobocka/PobockaForm.js
@@ -22,80 +22,83 @@ const PobockaForm = () => {
       <h2>Napište nám</h2>
       <p>Máte nějaké dotazy? Neváhejte se zeptat!</p>
       {/* show message after sending the form */}
-      <h3>{message}</h3>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/*  name */}
-        <label htmlFor="firstName">Jméno</label>
-        <input
-          name="firstName"
-          ref={register({
-            required: "Toto pole je nutné vyplnit.",
-            minLength: { value: 3, message: "Jméno musí být delší." },
-            pattern: {
-              value: /^[A-Za-zěščřžýáíéů]+$/i,
-              message: "Speciální znaky a čísla nejsou povoleny.",
-            },
-          })}
-        />
-        {errors.firstName && (
-          <p className="form-error">{errors.firstName.message}</p>
-        )}
-        {/* lastname */}
-        <label htmlFor="lastName">Příjmení </label>
-        <input
-          name="lastName"
-          ref={register({
-            required: "Toto pole je nutné vyplnit.",
-            minLength: { value: 3, message: "Jméno musí být delší." },
-            pattern: {
-              value: /^[A-Za-zěščřžýáíéů]+$/i,
-              message: "Speciální znaky a čísla nejsou povoleny.",
-            },
-          })}
-        />
-        {errors.lastName && (
-          <p className="form-error">{errors.lastName.message}</p>
-        )}
-        {/* phone contact */}
-        <label htmlFor="mobil">Telefon</label>
-        <input
-          name="mobil"
-          ref={register({
-            required: "Toto pole je nutné vyplnit.",
-            minLength: { value: 9, message: "Telefoní číslo je moc krátké" },
+      {message !== "" ? (
+        <h3>{message}</h3>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/*  name */}
+          <label htmlFor="firstName">Jméno</label>
+          <input
+            name="firstName"
+            ref={register({
+              required: "Toto pole je nutné vyplnit.",
+              minLength: { value: 3, message: "Jméno musí být delší." },
+              pattern: {
+                value: /^[A-Za-zěščřžýáíéů]+$/i,
+                message: "Speciální znaky a čísla nejsou povoleny.",
+              },
+            })}
+          />
+          {errors.firstName && (
+            <p className="form-error">{errors.firstName.message}</p>
+          )}
+          {/* lastname */}
+          <label htmlFor="lastName">Příjmení </label>
+          <input
+            name="lastName"
+            ref={register({
+              required: "Toto pole je nutné vyplnit.",
+              minLength: { value: 3, message: "Jméno musí být delší." },
+              pattern: {
+                value: /^[A-Za-zěščřžýáíéů]+$/i,
+                message: "Speciální znaky a čísla nejsou povoleny.",
+              },
+            })}
+          />
+          {errors.lastName && (
+            <p className="form-error">{errors.lastName.message}</p>
+          )}
+          {/* phone contact */}
+          <label htmlFor="mobil">Telefon</label>
+          <input
+            name="mobil"
+            ref={register({
+              required: "Toto pole je nutné vyplnit.",
+              minLength: { value: 9, message: "Telefoní číslo je moc krátké" },
 
-            maxLength: { value: 9, message: "Telefoní číslo je moc dlouhé." },
-          })}
-        />
-        {errors.mobil && <p className="form-error">{errors.mobil.message}</p>}
-        {/* email contact */}
-        <label htmlFor="email">Emailová adresa</label>
-        <input
-          name="email"
-          ref={register({
-            required: "Toto pole je nutné vyplnit.",
-            minLength: { value: 8, message: "Email je moc krátký" },
-          })}
-        />
-        {errors.email && <p className="form-error">{errors.email.message}</p>}
+              maxLength: { value: 9, message: "Telefoní číslo je moc dlouhé." },
+            })}
+          />
+          {errors.mobil && <p className="form-error">{errors.mobil.message}</p>}
+          {/* email contact */}
+          <label htmlFor="email">Emailová adresa</label>
+          <input
+            name="email"
+            ref={register({
+              required: "Toto pole je nutné vyplnit.",
+              minLength: { value: 8, message: "Email je moc krátký" },
+            })}
+          />
+          {errors.email && <p className="form-error">{errors.email.message}</p>}
 
-        <label htmlFor="notes">Poznámky</label>
-        <input
-          className="notes"
-          name="notes"
-          ref={register({
-            required: "Toto pole je nutné vyplnit.",
-          })}
-        />
+          <label htmlFor="notes">Poznámky</label>
+          <input
+            className="notes"
+            name="notes"
+            ref={register({
+              required: "Toto pole je nutné vyplnit.",
+            })}
+          />
 
-        <p>
-          Odesláním formuláře souhlasíte se zpracováním{" "}
-          <a href="/osobniudaje">osobních údajů</a>.
-        </p>
-        <div className="buttonWrapper">
-          <button type="submit">Odeslat</button>
-        </div>
-      </form>
+          <p>
+            Odesláním formuláře souhlasíte se zpracováním{" "}
+            <a href="/osobniudaje">osobních údajů</a>.
+          </p>
+          <div className="buttonWrapper">
+            <button type="submit">Odeslat</button>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
